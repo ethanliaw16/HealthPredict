137d58bf4c321f7021b55f772ca8d7fea4a8e6fd
Ethan <ethan.liaw8@gmail.com>
2020-09-10 09:49:12 -0400

    basic gan for pima indian dataset augmentation, not complete

---
+++ generate_fake_data.py
@@ -0,0 +1,16 @@
+from keras.models import load_model
+from numpy.random import randn, rand
+
+def generate_latent_points(latent_dimension, num_samples):
+    x_input = randn(latent_dimension * num_samples)
+    x_input = x_input.reshape(num_samples, latent_dimension)
+    return x_input
+
+def print_generated_data(generated, n):
+    for i in range(n):
+        print(generated[i])
+
+model = load_model('generator_model_1500.h5')
+X = model.predict(generate_latent_points(3,5))
+
+print(X)
