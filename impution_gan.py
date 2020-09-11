8015d6db8e54b33dd23617ace2df64a7fdf234bc
Ethan <ethan.liaw8@gmail.com>
2020-09-11 11:03:15 -0400

    Added data preprocessing for ehr data, created csvs without missing values

--- impution_gan.py
+++ impution_gan.py
@@ -5,6 +5,29 @@ from keras.optimizers import Adam
 from numpy.random import randint, uniform, randn, rand
 from sklearn.preprocessing import minmax_scale
 import numpy as np
+import pandas as pd
 import random
 
-dataset = loadtxt('diabetes.csv', delimiter=',')
+dataset = pd.read_csv('ehr_diabetes.csv', delimiter=',')
+print(dataset.shape)
+
+print(dataset["race"].head())
+dataset_weight_known = dataset[dataset["weight"] != '?']
+print(dataset_weight_known.shape)
+print(dataset_weight_known.head())
+
+columns_with_many_missing_values = []
+
+for(column_name, column_data) in dataset.iteritems():
+    num_missing = (dataset[column_name] == '?').sum()
+    if num_missing > 100:
+        columns_with_many_missing_values.append((column_name, num_missing))
+
+print(columns_with_many_missing_values)
+
+dataset_less_columns = dataset.drop(columns=['payer_code','medical_specialty','diag_2','diag_3'])
+print('Shape of dataset with dropped columns: ', dataset_less_columns.shape)
+dataset_no_missing = dataset_less_columns[(dataset_less_columns != '?').all(axis=1)]
+print('Shape of data without missing values: ', dataset_no_missing.shape)
+
+dataset_no_missing.to_csv('ehr_diabetes_no_missing_3k.csv')
