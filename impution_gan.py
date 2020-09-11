ced749a2106bd2e20c4676c360cb23bb18c77a9d
Ethan <ethan.liaw8@gmail.com>
2020-09-11 14:53:17 -0400

    changed preprocessing to include diagnoses(outcomes)

--- impution_gan.py
+++ impution_gan.py
@@ -25,9 +25,16 @@ for(column_name, column_data) in dataset.iteritems():
 
 print(columns_with_many_missing_values)
 
-dataset_less_columns = dataset.drop(columns=['payer_code','medical_specialty','diag_2','diag_3'])
+dataset_less_columns = dataset.drop(columns=['payer_code','medical_specialty'])
 print('Shape of dataset with dropped columns: ', dataset_less_columns.shape)
 dataset_no_missing = dataset_less_columns[(dataset_less_columns != '?').all(axis=1)]
 print('Shape of data without missing values: ', dataset_no_missing.shape)
+print('Diagnosis 1: ', (dataset_no_missing['diag_1'].str.match('250')).sum())
+print('Diagnosis 2: ', (dataset_no_missing['diag_2'].str.match('250')).sum())
+print('Diagnosis 3: ', (dataset_no_missing['diag_3'].str.match('250')).sum())
+print(dataset_no_missing[dataset_no_missing['diag_3'].str.match('250')]['diag_3'])
+#print(dataset_no_missing['diag_2'].head(10))
+#print(dataset_no_missing['diag_1'].head())
+#print(dataset_no_missing['diag_1'].value_counts())
 
 dataset_no_missing.to_csv('ehr_diabetes_no_missing_3k.csv')
