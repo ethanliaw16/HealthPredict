3a06c03e037aad0932cf0bb2550ff8f5c4f98e01
Amy Yu <asy27@case.edu>
2020-10-06 19:51:12 -0400

    created heart disease inputs form and page

--- flask_apps/app/__init__.py
+++ flask_apps/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8d5eb5a677571260474b96e59afd5'
+app.config['SECRET_KEY'] = '5aOlWgefqlYW09ZZfwJL7g2y3e5bR89x'
 
 from app import routes
