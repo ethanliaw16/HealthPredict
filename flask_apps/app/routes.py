bf81e8025e87f3269c4193bdd6163933ad802428
Ethan <ethan.liaw8@gmail.com>
2020-10-11 17:45:52 -0400

    connected validated form to prediction model, prediction to output render

--- flask_apps/app/routes.py
+++ flask_apps/app/routes.py
@@ -4,7 +4,7 @@ import pandas as pd
 import lightgbm as lgb
 import pickle
 from app.forms import DiabetesInputsForm, HeartDiseaseInputsForm
-
+from app.diabetes_form_map import map_form_to_vector
 # @app.route('/home', methods=['GET'])
 # def get_data():
 #     some_data = pd.read_csv('../ehr_diabetes_no_missing_3k.csv')
@@ -24,19 +24,14 @@ def about():
 def inputDiabetesInfo():
     form = DiabetesInputsForm()
     if form.validate_on_submit():
-        print('age ', form.age.data)
-        print('gender ', form.gender.data)
-        print('height ', form.height.data)
-        print('smoking history ', form.smoking_history.data)
-        print('smoking status ', form.smoking_status.data)
         # send to GAN
-        # return redirect(url_for('diabetesoutput')) this should go to output page instead
-    default_input = [[1970, 67, 180, 28.2, 120, 80, 1, 1, 0, 0, 0, 1, 0, 0]]
-    gbm_predictor = pickle.load(open('../trained_models/gbm_predictor.txt', 'rb'))
-
-    outcome = gbm_predictor.predict(default_input, num_iteration=gbm_predictor.best_iteration)
-    print('Chance of type 2 Diabetes: ', outcome[0])
-
+        user_input = map_form_to_vector(form)
+        gbm_predictor = pickle.load(open('../trained_models/gbm_predictor.txt', 'rb'))
+        outcome = gbm_predictor.predict([user_input], num_iteration=gbm_predictor.best_iteration)
+        print('Chance of type 2 Diabetes: ', outcome[0])
+        output_value = round(100 * outcome[0], 2)
+        return render_template('output_diabetes.html', prediction=output_value) #this should go to output page instead
+    
         # return redirect(url_for('diabetesoutput'))
     return render_template('input_diabetes.html', title='Diabetes Inputs', form=form)
 
